e19d3d14c43ddea57ee48bfabe9c9730c9731e87
hly <hly920928@gmail.com>
2019-04-26 22:48:28 +0800

    Vue_cli installation and introduction

---
+++ FrontEnd_tool_note.ps1
@@ -0,0 +1,30 @@
+﻿E:
+cd E:\Programming_Project\FrontEndTool_test
+pwd
+cd ..
+ls
+ls -r
+cat test_1.txt
+mkdir Folder_2
+echo context_test >newFile.txt
+rm newFile.txt
+test_1.txt test_1_copy.txt
+mv Folder_1 Folder_2
+npm install jquery --save
+npm install lodash --save-dev
+npm list
+npm install jquery -g
+npm uninstall jquery -g
+npm list -g
+npm uninstall jquery --save-dev
+npm install
+npm install babel-cli -g
+npm install babel-preset-es2015 -g
+npm list -g --depth 0
+npm init -y
+babel ./src/es6_1.js -o ./dist/es5_1.js
+npm install css-loader style-loader --save-dev
+
+vue init simple vue_project_1
+vue init webpack-simple proj_vue_webpack_simple_1
+vue init webpack proj_vue_webpack_full_1
---
+++ proj_vue_webpack_simple_1/README.md
@@ -0,0 +1,18 @@
+# proj_vue_webpack_simple_1
+
+> vue_webpack_test
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+```
+
+For detailed explanation on how things work, consult the [docs for vue-loader](http://vuejs.github.io/vue-loader).
