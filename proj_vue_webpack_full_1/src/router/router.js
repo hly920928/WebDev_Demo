168fe8c1eb72df458cddd51e1e3f678970eba81e
hly <hly920928@gmail.com>
2019-05-05 21:10:01 +0800

    Vue Cli : Redirect and Lazy Loading

--- proj_vue_webpack_full_1/src/router/router.js
+++ proj_vue_webpack_full_1/src/router/router.js
@@ -2,10 +2,20 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 //components
 import comp_1 from "../components/comp_1"
-import comp_2 from "../components/comp_2"
-import comp_3 from "../components/comp_3"
+const  comp_2=()=> import("../components/comp_2");  /* using function to lazyloading */
+const  comp_3=()=> import("../components/comp_3");
+ 
 Vue.use(VueRouter);
 const routes=[
+   {name:"root",path:"/",
+   redirect: /* redirect root to comp_1_name */
+   {
+      name:"comp_1_name",
+     params:{
+      msg_1:"redirect from root"
+     }
+   }
+},
    {name:"comp_1_name",path:"/comp_1/:msg_1",component:comp_1},
    {path:"/comp_2",component:comp_2},
    {path:"/comp_3",component:comp_3}
